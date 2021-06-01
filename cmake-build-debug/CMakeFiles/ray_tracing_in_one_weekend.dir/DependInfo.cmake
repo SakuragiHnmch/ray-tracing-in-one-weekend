d71da1e750e9d9c3c4ab257e4581bfe2862f5349
junkang <junkang.lan@ivydad.com>
2021-06-01 14:50:47 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/ray_tracing_in_one_weekend.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/junkang/Documents/GrandLine/ray-tracing-in-one-weekend/main.cpp" "/Users/junkang/Documents/GrandLine/ray-tracing-in-one-weekend/cmake-build-debug/CMakeFiles/ray_tracing_in_one_weekend.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
