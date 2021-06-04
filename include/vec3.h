312a0dd3d9690456bb7101e49112f3f5604ca1df
junkang <754089122@qq.com>
2021-06-04 17:08:40 +0800

    material light

--- include/vec3.h
+++ include/vec3.h
@@ -57,6 +57,12 @@ public:
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 
+    bool near_zero() const {
+        //return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
 public:
     double e[3];
 };
@@ -107,6 +113,10 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
@@ -119,8 +129,16 @@ inline vec3 random_in_unit_sphere() {
     }
 }
 
-inline vec3 random_unit_vector() {
+vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0)
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
+
 
 #include <iostream>
 
@@ -12,13 +14,16 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
     //if we have exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0) {
-        return color(0, 0,0);
+        color black = color(0, 0, 0);
+        return black;
     }
 
     if (world.hit(r, 0.001, infinity, rec)) {
-        //get a random point in unit radius sphere
-        point3 target = rec.p + rec.normal + random_unit_vector();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * ray_color(scattered, world, depth - 1);
+        }
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -35,8 +40,16 @@ int main() {
 
     //world
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    //定义不同材质的反射率
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     //Camera
     camera cam;
